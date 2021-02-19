d27861081905f807a26ea7fa636cb799b298211a
Hugues <hugues.gallier@ensae-paristech.fr>
2021-02-19 15:40:34 +0100

    Creation - facilitate imports

---
+++ Utils/__init__.py
@@ -0,0 +1,4 @@
+from .utils import PATH_TO_CSV, SIZE
+from .grid import Grid
+
+__all__ = ["Grid", "PATH_TO_CSV", "SIZE"]
---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .Utils import Grid, PATH_TO_CSV, SIZE
+from .Backtrack import BacktrackSolver
+
+__all__ = ["Grid", "BacktrackSolver", "PATH_TO_CSV", "SIZE"]
