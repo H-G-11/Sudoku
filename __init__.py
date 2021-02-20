bf6fe4dafa175b5809da1656ac9af2f17ab4532d
Hugues <hugues.gallier@ensae-paristech.fr>
2021-02-20 16:09:59 +0100

    Correct typo

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from .Utils import Grid, PATH_TO_CSV, SIZE, UnsolvableError
 from .Backtrack import BacktrackSolver
 
-__all__ = ["Grid", "BacktrackSolver", "PATH_TO_CSV", "SIZE", UnsolvableError]
+__all__ = ["Grid", "BacktrackSolver", "PATH_TO_CSV", "SIZE", "UnsolvableError"]
